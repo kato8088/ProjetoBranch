54b5a9e35ae03740b58cd771042cbe1b0c0472ad
Kato <kato8088@github.com>
2024-10-03 11:24:50 -0300

    funcoes

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 // estrutura de repetição for
+console.log("for")
 for (let i=0; i<10; i++){
     console.log(i);
 }
 
 // estrutura de repetição while
+console.log("while")
 let f = 0;
 while (f < 10){
     console.log(f);
     f++;
 }
+
+// do while
+let w = 0;
+
+console.log("do while")
+do {
+    console.log(w);
+    w++;
+}
+while (w<5);
+
+//funções
+
+function ola(nome){
+    //string literal (crase ``) ou template string (${}), chama a variável
+    console.log(`seja bem vindo, ${nome}`);
+    alert(`Seja bem vindo ${nome}`);
+    document.write(`seja bem vindo ${nome}`);
+}
+
+ola("sergiu"); 
